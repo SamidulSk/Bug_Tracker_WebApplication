c96a9f7ff62a0af40e83551df4743244e4766f50
SamidulSk <samidulsk@@>
2025-06-15 20:20:20 +0530

    finalizing project with dockerization❤️👍

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',   // allow external access (for Docker)
+    port: 5173,         // expose Vite port
+  },
 })
